361cd0abd490946f2a10a057da9a4a91051b5154
Amjad1972 <34518941+Amjad1972@users.noreply.github.com>
2018-02-06 14:32:27 +0200

    Add files via upload

---
+++ week1/JavaScript.js
@@ -0,0 +1,19 @@
+let x = 10;
+console.log(x);
+
+let y = Math.round(7.25);
+console.log(y);
+
+let color = ['red', 'blue', 'green'];
+
+console.log(color.length);
+
+let z = "9";
+let s = "Hello";
+if (typeof z === typeof s) {
+console.log("SAME TYPE");
+}
+
+let a = 7;
+a = a % 3;
+console.log(a);
